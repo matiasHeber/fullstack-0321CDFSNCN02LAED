09658089baa818a9b302531bf3ffa2c78ae775cc
Uriel Sosa <sosa.uriel1999@gmail.com>
2021-07-09 20:41:09 -0300

    agregamos clase 37

---
+++ m7-jsfront/c37-manipulando-elementos-con-jsfront/app/public/js/index.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('.tituloPrincipal');
+console.log(titulo);
+
+let h1 = 'Digital Netflix';
+
+titulo.innerText = h1;
+
+titulo.classList.remove('tituloPrincipal');
+titulo.classList.add('color-red');
